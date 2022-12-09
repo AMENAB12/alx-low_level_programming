dbacc5abe59e924ece29fbb609cc56993efad395
AMENAB12 <lalatube71@gmail.com>
2022-12-09 05:58:38 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,15 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: %lu byte(S)\n", (unsigned long)size of (a));
+printf("Size of a char: %lu byte(S)\n", (unsigned long)size of(a));
 
-printf("Size of a int: %lu byte(S)\n", (unsigned long)size of (b));
+printf("Size of a int: %lu byte(S)\n", (unsigned long)size of(b));
 
-printf("Size of a long int: %lu byte(S)\n", (unsigned long)size of (c));
+printf("Size of a long int: %lu byte(S)\n", (unsigned long)size of(c));
 
-printf("Size of a long long int: %lu byte(S)\n", (unsigned long)size of (d));
+printf("Size of a long long int: %lu byte(S)\n", (unsigned long)size of(d));
 
-printf("Size of a float: %lu byte(S)\n", (unsigned long)size of (f));
+printf("Size of a float: %lu byte(S)\n", (unsigned long)size of(f));
 
 return (0);
 }
