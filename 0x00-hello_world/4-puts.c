d73de4fab58fe14e0689706b84128786b728e3fe
AMENAB12 <lalatube71@gmail.com>
2022-12-09 04:50:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#includ <stdio.h> 
+#include <stdio.h> 
 /*
- * main - print a line of code using puts
+ * main -print a line of code using puts
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
