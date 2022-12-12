6120e6b9caeacde0cadd1c240ef3103229d1be18
AMENAB12 <lalatube71@gmail.com>
2022-12-12 10:30:17 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower cases
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i = '26'; i++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
