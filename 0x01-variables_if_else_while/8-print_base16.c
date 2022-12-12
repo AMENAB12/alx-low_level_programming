fa5e43992fa9093231cf831b5197f920e746dee0
AMENAB12 <lalatube71@gmail.com>
2022-12-12 12:03:08 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char ch;
+
+		for (ch = 'a'; ch <= '9'; ch++)
+		{
+			putchar(ch);
+		}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+	putchar(ch);
+	}
+putchar(ch);
+return (0);
+}
