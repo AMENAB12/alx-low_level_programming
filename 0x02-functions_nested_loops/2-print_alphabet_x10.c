0b75ce9a61c5245407bd4363311db7dd0d7ee40c
AMENAB12 <lalatube71@gmail.com>
2022-12-13 15:11:08 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-	char letter = '10 * 'a';
+	int count = 0;
+	char letter;
 
-	for (letter = '10 * 'a'; '10 * 'a' <= '10 * 'z'; letter++);
-
-	_putchar(letter);
-
-	_putchar('\n');
+	while (count++ < 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+}
 }
