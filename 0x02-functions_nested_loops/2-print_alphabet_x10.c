c93268816f9bb4942954614a272ee96bada911c3
AMENAB12 <lalatube71@gmail.com>
2022-12-13 14:53:38 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = '10 * 'a';
+
+	for (letter = '10 * 'a'; '10 * 'a' <= '10 * 'z'; letter++);
+
+	_putchar(letter);
+
+	_putchar('\n');
+}
