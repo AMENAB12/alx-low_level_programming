64096644afeb1694723e373555062415a4c256e7
AMENAB12 <lalatube71@gmail.com>
2022-12-13 15:48:21 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Write a function that checks for alphabetic character.
+ * @c: the char
+ * Return: 1 if is lower or upper, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (C >= 'A' && C <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
