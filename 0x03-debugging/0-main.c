53cec15b724898d383ffb26f02828199fa091e41
AMENAB12 <lalatube71@gmail.com>
2022-12-15 10:54:30 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - postitive_or_negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+	return (0);
+}
