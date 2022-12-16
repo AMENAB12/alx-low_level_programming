fa87ee48e503983948d1843a89a9a1065ede34c1
AMENAB12 <lalatube71@gmail.com>
2022-12-16 13:41:18 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * Return: number 0-9
+ * Return: number0-9
  */
 
 void print_numbers(void)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
