0798070ad5d7cffb742955be75debd1f088b1501
AMENAB12 <lalatube71@gmail.com>
2022-12-16 18:05:32 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,27 +7,17 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		m = -n
 	}
 	else
 	{
-		m = n;
+	m = n;
 	}
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
-		{
-			_putchar(((m / count) % 10) + 48);
-		}
+	if (m / 10 != 0)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
 }
