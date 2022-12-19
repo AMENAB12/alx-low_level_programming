0a192fc4c7c69a34dc4088860c4a6424b5018a9b
AMENAB12 <lalatube71@gmail.com>
2022-12-19 14:47:20 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - write a pointer int as parameter and updates the value
+ * @n: int for input pointer
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
