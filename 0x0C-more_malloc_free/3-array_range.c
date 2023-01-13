25cfc67e7e1bf665a52c57eb01e7d322d7e5ba11
AMENAB12 <lalatube71@gmail.com>
2023-01-13 12:38:28 -0800

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,11 +10,21 @@
  */
 int *array_range(int min, int max)
 {
-int *ptr;
-if(min > max)
-return (NULL);
-ptr = malloc(sizeof(int) * (max-min));
-if (ptr == NULL)
-return (NULL);
-return (ptr);
+	int *ptr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
 }
