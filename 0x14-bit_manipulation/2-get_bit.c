79393f3130d0b8873e59959271f84a39e860ad7a
AMENAB12 <lalatube71@gmail.com>
2023-02-02 06:54:54 -0800

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - value of a bit at index
+ * @n: gotten git num
+ * @index: gotten bit index
+ * Return: value of bit or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int max = 0x01;
+
+	max <<= index;
+	if (max == 0)
+		return (-1);
+
+	if ((n & max))
+		return (1);
+	else
+		return (0);
+}
